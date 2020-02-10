e088748394bd28ed4a9f7df1f0890f658209ad60
OmarVelasco10 <60717132+OmarVelasco10@users.noreply.github.com>
2020-02-10 16:30:51 -0600

    Calificación
    De un numero,te da calificación en sistema Americano.

---
+++ Calificacion.js
@@ -0,0 +1,20 @@
+var x= Math.floor(Math.random()*10+1);
+
+if(x==10){
+    console.log("A");
+}
+else if(x==9){
+    console.log("B");
+}
+else if(x==8){
+    console.log("C");
+}
+else if(x==7){
+    console.log("D");
+}
+else if(x==6){
+    console.log("E");
+}
+else{
+    console.log("F");
+}
