52c4535ecb23f44bc3ac0ed594a7aa714a79e3de
OmarVelasco10 <60717132+OmarVelasco10@users.noreply.github.com>
2020-05-13 20:30:38 -0500

    App.js

---
+++ App.js
@@ -0,0 +1,23 @@
+const miInfoo=new Array();
+
+const btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener{"click", e =>{
+  let nombre=document.getElementById("txtNombre").value;
+  let descripcion=document.getElementById("txtDescripcion").value;
+  let cantidad=document.getElementById("txtCantidad").value;
+  cantidad=parseInt(cantidad);
+  let costo=parseInt(document.getElementById("txtCosto").value);
+
+  let product=new Producto(nombre, descripcion, cantidad, costo);
+
+  let details = document.getElementById("details");
+  details.innerHTML+= `
+  <div>
+  <p><strong>ID ${product.id} - </strong${product.nombre}</p>
+  <p>Desc: ${product.descripcion}</p>
+  <p>Valor Total:${product.valorMercancia}</p>
+  <p>${product.showTextInfo()}</p>
+  </div>
+  `;
+  console.log(product.showInfo);
+});
